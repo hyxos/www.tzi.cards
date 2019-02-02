ced9331b8b7b45df979e1cfcb3d8c174904e2d3c
hyxos <dw@1hx.ca>
2019-02-02 14:40:26 -0500

    moving entries into their own template

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/content/entries`,
+        name: 'entries',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/images`,
         name: 'images',
       },
