25579b1c93f1d485ca4ff9a593ed4ee7dcd34905
hyxos <dw@1hx.ca>
2019-02-05 20:02:21 -0500

    removing mailchimp script

--- src/html.js
+++ src/html.js
@@ -13,18 +13,6 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           {this.props.headComponents}
-          <script id="mcjs"
-            dangerouslySetInnerHTML={{
-              __html: `
-              !function(c,h,i,m,p){m=c.createElement(h),
-                p=c.getElementsByTagName(h)[0],
-                m.async=1
-                ,m.src=i,
-                p.parentNode.insertBefore(m,p)}
-                (document,"script","https://chimpstatic.com/mcjs-connected/js/users/1cb0b142f71bdb1ca1c6b9f3b/641dcf5e7f4844264aa1dd798.js");
-                  `,
-            }}
-          />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
