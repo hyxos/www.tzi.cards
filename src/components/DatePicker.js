a8c261e4051a3c05db5887e00f359f2f6357c4b3
hyxos <dw@1hx.ca>
2019-03-04 12:39:20 -0500

    refactored DatePicker to use hooks

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { css } from '@emotion/core'
 import Moment from 'moment'
 import { extendMoment } from 'moment-range';
@@ -27,69 +27,63 @@ label {
 }
 `
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-const optionMonths = months.map((month) =>
-  <option key={month} value={month}>{month}</option>
-)
-export default class DatePicker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new moment(),
-      month: null,
-      day: null,
-      year: null
-    }
+function DatePicker () {
+  const [date, setDate] = useState(new moment())
+  const [month, setMonth] = useState(date.format('MMMM'))
+  const [day, setDay] = useState(date.format('D'))
+  const [year, setYear] = useState(date.format('YYYY'))
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const optionMonths = months.map((month) =>
+    <option key={month} value={month}>{month}</option>
+  )
 
-    this.handleChange = this.handleChange.bind(this);
+  function handleMonthChange(e) {
+    setMonth(e.target.value)
   }
 
-  componentWillMount() {
-    this.setState({ 
-      month: this.state.date.format('MMMM'),
-      day: this.state.date.format('D'),
-      year: this.state.date.format('YYYY')
-    })
+  function handleDayChange(e) {
+    setDay(e.target.value)
   }
 
-  handleChange(event) {
-    this.setState({[event.target.name]: event.target.value});
+  function handleYearChange(e) {
+    setYear(e.target.value)
   }
 
-  render() {
-    return (
-      <div css={datePickerlStyles}>
-        <div className="container">
-          <form action="#">
-            <span>
-              <label htmlFor="month">Month</label>
-              <select name="month" defaultValue={this.state.month} onChange={this.handleChange}>
-                {optionMonths}
-              </select>
-            </span>
-            <span>
-              <label htmlFor="day">Day</label>
-              <input name="day" type="text" defaultValue={this.state.day} onChange={this.handleChange}></input>
-            </span>
-            <span>
-              <label htmlFor="year">Year</label>
-              <input name="year" type="text" defaultValue={this.state.year} onChange={this.handleChange}></input>
-            </span>
-          </form>
-        </div>
-        <div className="container">
-          <table>
-            <tbody>
-              <tr>
-                <td>{this.state.month}</td>
-                <td>{this.state.day}</td>
-                <td>{this.state.year}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+  useEffect(() => {
+    setDate(moment(`${month + " " +  day + " " + year}`))
+  })
+
+  return (
+    <div css={datePickerlStyles}>
+      <div className="container">
+        <form action="#">
+          <span>
+            <label htmlFor="month">Month</label>
+            <select name="month" defaultValue={month} onChange={handleMonthChange}>
+              {optionMonths}
+            </select>
+          </span>
+          <span>
+            <label htmlFor="day">Day</label>
+            <input name="day" type="text" defaultValue={day} onChange={handleDayChange}></input>
+          </span>
+          <span>
+            <label htmlFor="year">Year</label>
+            <input name="year" type="text" defaultValue={year} onChange={handleYearChange}></input>
+          </span>
+        </form>
       </div>
-    )
-  }
+      <div className="container">
+        <table>
+          <tbody>
+            <tr>
+              <td>{date.format("MMM Do YYYY")}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
+export default DatePicker
